0aa861c02943fb1202f9a24ce4ffe6af856ec849
Southclaws <Southclaws@gmail.com>
2017-07-28 17:55:13 +0100

    remove semantic

--- index.tsx
+++ index.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import 'semantic-ui-css/semantic.min.css';
 
 import App from './components/app'
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById("root")
 );
