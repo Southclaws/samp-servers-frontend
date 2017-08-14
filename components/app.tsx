c1a57ccf29740ec96e53616e6a0d76a58c1dc5ac
Southclaws <Southclaws@gmail.com>
2017-08-14 20:45:35 +0100

    implemented some form of responsiveness and a statistics panel to show the amount of servers and players

--- components/app.tsx
+++ components/app.tsx
@@ -1,7 +1,8 @@
 import * as URL from 'url'
 import * as React from "react";
 import { SyntheticEvent, Component } from "react";
-import { Container, Grid, Header, Input, Button, Popup, Icon, Divider } from 'semantic-ui-react';
+import { Container, Segment, Grid, Header, List, Input, Button, Popup, Icon, Divider, Statistic } from 'semantic-ui-react';
+import Device from 'react-device'
 
 import { ServerFull, ServerCore, decodeServerCore } from "./server"
 import ServerList from "./list"
@@ -15,6 +16,7 @@ interface AppState {
     searchQuery: string
     addAddress: string
     addSuccess: boolean
+    isMobile: boolean
 }
 
 export default class App extends Component<AppProps, AppState> {
@@ -28,6 +30,7 @@ export default class App extends Component<AppProps, AppState> {
             searchQuery: "",
             addAddress: "",
             addSuccess: false,
+            isMobile: false,
         };
     }
     componentDidMount() {
@@ -115,22 +118,56 @@ export default class App extends Component<AppProps, AppState> {
         setTimeout(() => { this.setState({ addSuccess: false }) }, 2500)
     }
 
+    onDeviceChange(deviceInfo: any) {
+        if (deviceInfo.screen.width < 768 && !this.state.isMobile) {
+            this.setState({ isMobile: true })
+        } else {
+            this.setState({ isMobile: false })
+        }
+    }
+
     render() {
         if (this.state.servers === null) {
             return (<div></div>)
         }
+
+        let numServers = this.state.servers.length
+        let numPlayers = 0
+        this.state.servers.forEach(s => {
+            numPlayers += s.pc
+        });
+
         return (<Container>
-            <Grid columns="3" >
-                <Grid.Row>
-                    <Header as='h1' attached='top' inverted>
-                        SA:MP Servers
-                        <Header.Subheader>
-                            by southclaws
-                        </Header.Subheader>
-                    </Header>
-                    <Divider inverted />
+            <Device onChange={this.onDeviceChange.bind(this)} />
+            <Grid padded>
+                <Grid.Row columns="2">
+                    <Grid.Column width='6' >
+                        <Container>
+                            <Header size='huge' as='h1' inverted>SA:MP Servers
+                            <Header.Subheader>by southclaws</Header.Subheader>
+                            </Header>
+                        </Container>
+                    </Grid.Column >
+                    <Grid.Column >
+                        <Grid container columns="3" padded>
+                            <Grid.Row>
+                                <Grid.Column >
+                                    <Statistic label='Players' value={numPlayers} inverted='true' />
+                                </Grid.Column >
+                                {this.state.isMobile ? (null) :
+                                    (<Grid.Column >
+                                        <Divider vertical inverted>on</Divider>
+                                    </Grid.Column >)
+                                }
+                                <Grid.Column >
+                                    <Statistic label='Servers' value={numServers} inverted='true' />
+                                </Grid.Column >
+                            </Grid.Row>
+                        </Grid>
+                    </Grid.Column >
                 </Grid.Row>
-                <Grid.Row>
+                <Divider inverted />
+                <Grid.Row columns="3">
                     <Grid.Column >
                         <Input
                             inverted
@@ -185,7 +222,7 @@ export default class App extends Component<AppProps, AppState> {
                         />
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row>
+                <Grid.Row columns="1">
                     <ServerList servers={this.state.servers} filter={this.state.searchQuery} />
                 </Grid.Row>
             </Grid>
