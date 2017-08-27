9a2f03a62266f0936ee603d2ff48fdc8ae905b8f
Southclaws <Southclaws@gmail.com>
2017-08-27 12:04:42 +0100

    attempt fix webpack prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require("compression-webpack-plugin");
 
@@ -8,7 +9,7 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"production"'
         }),
-        new webpack.optimize.UglifyJsPlugin({
+        new UglifyJSPlugin({
             mangle: true,
             compress: {
                 warnings: false, // Suppress uglification warnings
