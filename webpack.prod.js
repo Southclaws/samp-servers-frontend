eb9bc37af547b293ac526422f2aad10a7f8e1108
Southclaws <Southclaws@gmail.com>
2017-08-27 13:12:34 +0100

    uglify seems like it just won't ever work... never mind

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"production"'
-        })
+        }),
+        // does not work, no minification :(
+        // new UglifyJSPlugin({
+        //     ecma: 6,
+        //     parse: {
+        //         ecma: 6
+        //     }
+        // })
     ]
 });
