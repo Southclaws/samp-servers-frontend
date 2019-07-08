d530cb637f3bcc01cf3ec4837b13e4190420ac4f
Barnaby Keene <hello@southcla.ws>
2019-07-08 18:25:33 +0100

    fixed ul item key prop being in the wrong node

--- src/components/ServerList.jsx
+++ src/components/ServerList.jsx
@@ -5,12 +5,9 @@ export default ({ servers }) => {
   return (
     <ul className="list pl0 mt0 center">
       {servers.map(server => (
-        <Link as={"/server/" + server.ip} href={`/server?ip=${server.ip}`}>
+        <Link key={server.ip} as={"/server/" + server.ip} href={`/server?ip=${server.ip}`}>
           <a className="link black">
-            <li
-              key={server.ip}
-              className="flex hover-bg-light-red items-center justify-between lh-copy pa2 ph0-l bb b--black-10"
-            >
+            <li className="flex hover-bg-light-red items-center justify-between lh-copy pa2 ph0-l bb b--black-10">
               <div className="pl2 overflow-hidden">
                 <span className="db black-70 measure truncate">{server.hn}</span>
                 <span className="db black-30 f6 measure truncate">{server.gm}</span>
